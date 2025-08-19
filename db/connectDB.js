81ceb08a4511f8cba82f8da4ba038f371ffb7dd4
hariomdhakad16 <hariomdhakad16122002@gmail.com>
2025-08-19 21:35:47 +0530

    one more

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Live_url = "mongodb+srv://hariom:Hariom123@cluster0.uqhrhpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const Live_url = "mongodb+srv://hariom:Hariom123@cluster0.uqhrhpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
    //  return mongoose.connect('mongodb://127.0.0.1:27017/coursebooking')
